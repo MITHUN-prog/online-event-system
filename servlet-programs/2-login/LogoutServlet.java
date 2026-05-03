c8200d21cd6c25b240b25001c82fd97adc55f8c0
MITHUN-prog <mithunchakravarthy.l.2024.aids@rajalakshmi.edu.in>
2026-05-03 11:43:03 +0530

    Create LogoutServlet.java

---
+++ servlet-programs/2-login/LogoutServlet.java
@@ -0,0 +1,20 @@
+package log;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+public class LogoutServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false);
+
+        if (session != null) {
+            session.invalidate();
+        }
+
+        response.sendRedirect("login.html");
+    }
+}
